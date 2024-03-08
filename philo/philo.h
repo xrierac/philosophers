dd4ffdeb68add18e5ad8b5a64cdeab9236bf55f3
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-08 12:56:30 +0200

    Add table struct

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:39:52 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/06 16:40:49 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/08 11:55:34 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,18 @@
 
 # include <unistd.h>
 # include <sys/time.h>
+
+typedef struct s_philo
+{
+}	t_philo
+
+typedef struct s_table
+{
+	int		nbrphilo;
+	long	tdie;
+	long	teat;
+	long	tsleep;
+	long	nbreat;
+}	t_table;
 
 #endif
