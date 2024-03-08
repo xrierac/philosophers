c910da89b23bfba33335d4c5e44795a90a2ee6e9
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-08 17:31:18 +0200

    Add function prototypes

--- philo/philo.h
+++ philo/philo.h
@@ -6,19 +6,20 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:39:52 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/08 11:55:34 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/08 17:26:07 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdlib.h>
 # include <unistd.h>
 # include <sys/time.h>
 
 typedef struct s_philo
 {
-}	t_philo
+}	t_philo;
 
 typedef struct s_table
 {
@@ -28,5 +29,13 @@ typedef struct s_table
 	long	tsleep;
 	long	nbreat;
 }	t_table;
+
+//Parsing//
+int		parse_input(char *argv[], int argc, t_table *table);
+size_t	ft_strlen(char *str);
+long	ft_atol(char *str);
+
+//Utils//
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
