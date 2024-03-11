d4c8219b73e21d8994de6a2ef291d1e1a54ad643
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-11 12:38:22 +0200

    Change variable names from table to sim

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,14 +6,22 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:41:00 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/09 16:23:57 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/11 12:30:53 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	exit_error(char *str)
+void	exit_success(t_sim *sim)
 {
+	free(sim);
+	exit(EXIT_SUCCESS);
+}
+
+void	exit_error(char *str, t_sim *sim)
+{
+	if (sim != NULL)
+		free(sim);
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(str, 2);
 	exit(EXIT_FAILURE);
@@ -21,11 +29,11 @@ void	exit_error(char *str)
 
 int	main(int argc, char *argv[])
 {
-	t_table	table;
+	t_sim	*sim;
 
 	if (argc != 5 && argc != 6)
-		exit_error("Wrong number of arguments\n");
-	if (parse_input(argv, argc, &table) < 0)
-		exit_error("Wrong arguments\n");
+		exit_error("Wrong number of arguments\n", NULL);
+	sim = init_sim(argc, argv);
+	exit_success(sim);
 	return (0);
 }
--- philo/src/parsing.c
+++ philo/src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:56:03 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/09 17:03:42 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/11 12:31:53 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ static long	get_parse(char *str)
 	return (nbr);
 }
 
-int	parse_input(char *argv[], int argc, t_table *table)
+int	parse_input(char *argv[], int argc, t_sim *sim)
 {
-	table->nbrphilo = get_parse(argv[1]);
-	table->tdie = get_parse(argv[2]);
-	table->teat = get_parse(argv[3]);
-	table->tsleep = get_parse(argv[4]);
-	table->nbreat = -1;
+	sim->n_phil = get_parse(argv[1]);
+	sim->tdie = get_parse(argv[2]);
+	sim->teat = get_parse(argv[3]);
+	sim->tsleep = get_parse(argv[4]);
+	sim->n_eat = -1;
 	if (argc == 6)
-		table->nbreat = get_parse(argv[5]);
-	if (table->nbrphilo <= 0 || table->tdie < 0 || table->teat < 0 || 
-			table->tsleep < 0 || table->nbreat < -1)
+		sim->n_eat = get_parse(argv[5]);
+	if (sim->n_phil <= 0 || sim->tdie < 0 || sim->teat < 0 || 
+			sim->tsleep < 0 || sim->n_eat < -1)
 		return (-1);
 	return (0);
 }
