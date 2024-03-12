a49130161cbdbadf36b036217ebbe2101b21949b
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-12 12:26:47 +0200

    Add functions to free memory

--- philo/src/main_utils.c
+++ philo/src/main_utils.c
@@ -6,11 +6,30 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 12:04:08 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/09 16:26:17 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/12 12:25:27 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+
+t_sim	*free_sim(t_sim *sim)
+{
+	int	i;
+
+	i = -1;
+	if (sim->phteam != NULL)
+	{
+		while (sim->phteam[++i])
+		{
+			pthread_mutex_destroy
+			free(sim->phteam[i]);
+		}
+		free(sim->phteam);
+	}
+	if (sim != NULL)
+		free(sim);
+	return (NULL);
+}
 
 void	ft_putstr_fd(char *s, int fd)
 {
