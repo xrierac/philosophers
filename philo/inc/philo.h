9bbcf62d29f6cacf337b09e41b2ceab63764ce7d
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-13 14:23:55 +0200

    Add enums and definitions

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:39:52 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/12 12:07:50 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/13 14:06:23 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,61 @@
 # include <pthread.h>
 # include <stdio.h>
 
+# define ERR_MALLOC 	"Memory allocation failed\n"
+# define ERR_ARG_NUM	"Invalid number of arguments\n"
+# define ERR_ARG		"Invalid argument\n"
+# define ERR_MUTEX		"Mutex failed\n"
+
 typedef struct s_philo
 {
 	pthread_t		thread;
 	int				id;
 	int				n_meals;
 	int				last_meal;
+	int				forks[2];
 	pthread_mutex_t	meal_lock;
 }	t_philo;
 
-typedef struct s_sim
+typedef struct s_table
 {
 	int		n_phil;
 	int		tdie;
 	int		teat;
 	int		tsleep;
-	int		n_eat;
+	int		must_eat;
 	int		start_time;
-	t_philo	**phteam;
-}	t_sim;
+	t_philo	**philos;
+}	t_table;
+
+typedef enum e_status
+{
+	DIED = 0,
+	EATING = 1,
+	SLEEP = 2,
+	THINK = 3,
+	GOT_FORK = 4,
+	INIT = 1,
+	DESTROY = 2
+}	t_status;
 
 //Initialize//
-t_sim	*init_sim(int argc, char *argv[]);
-t_philo	**init_team(t_sim *sim);
+t_table	*init_table(int argc, char *argv[]);
+t_philo	**sit_philos(t_table *table);
+void	init_philos(t_table *table);
 
 //Parsing//
-int		parse_input(char *argv[], int argc, t_sim *sim);
+int		parse_input(char *argv[], int argc, t_table *table);
 size_t	ft_strlen(char *str);
 long	ft_atol(char *str);
 
+//Routines//
+void	philo_routine(void *arg);
+
 //Utils//
 void	ft_putstr_fd(char *s, int fd);
-t_sim	*free_sim(t_sim *sim);
+t_table	*free_table(t_table *table);
 
 //Exit//
-void	exit_error(char *str, t_sim *sim);
+void	exit_error(char *str, t_table *table);
 
 #endif
