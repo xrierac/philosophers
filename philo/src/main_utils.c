14c9d6ff71796c39eb682c038d3b9b4821615bb4
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-14 11:27:36 +0200

    Delete free_sim function

--- philo/src/main_utils.c
+++ philo/src/main_utils.c
@@ -6,30 +6,11 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 12:04:08 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/12 13:43:11 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/14 10:27:13 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
-
-t_sim	*free_sim(t_sim *sim)
-{
-	int	i;
-
-	i = -1;
-	if (sim->phteam != NULL)
-	{
-		while (sim->phteam[++i])
-		{
-			pthread_mutex_destroy(&sim->phteam[i]->meal_lock);
-			free(sim->phteam[i]);
-		}
-		free(sim->phteam);
-	}
-	if (sim != NULL)
-		free(sim);
-	return (NULL);
-}
 
 void	ft_putstr_fd(char *s, int fd)
 {
