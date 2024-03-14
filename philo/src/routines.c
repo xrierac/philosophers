7fe7526566b741997c046cb4b3812b35853d1185
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-14 16:51:19 +0200

    Add version of routine files

--- philo/src/routines.c
+++ philo/src/routines.c
@@ -6,22 +6,25 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:04:28 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/13 14:15:21 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/14 16:49:16 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/philos.h"
+#include "../inc/philo.h"
 
-void	philo_routine(void *arg)
+void	*philo_routine(void *arg)
 {
+	
 	t_philo	*philo;
 
 	philo =  arg;
+	/*
 	if (pthread_mutex_lock(&philo->meal_lock)
 		;
 			//exit_error(DFSHDAFSKHDFHSKKADS);
 	philo->last_meal = START_TIME!!!!!
 	if (pthread_mutex_unlock(&philo->meal_lock)
 		;
-
+*/
+	return (NULL);
 }
