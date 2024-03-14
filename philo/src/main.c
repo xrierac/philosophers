51d39606e45b4748f334c73bfe2a10c29744d9cc
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-14 16:51:41 +0200

    Include start function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,19 +6,12 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:41:00 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/14 15:56:36 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/14 16:37:12 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-/*
-void	start_meal(t_table *table)
-{
-	mutexes(table, INIT);
-
-}
-*/
 int	main(int argc, char *argv[])
 {
 	t_table	table;
@@ -26,6 +19,8 @@ int	main(int argc, char *argv[])
 	if (argc != 5 && argc != 6)
 		return (ft_exit(ERR_ARG_NUM, NULL, ERROR));
 	if (init_table(argc, argv, &table) == -1)
+		return (-1);
+	if (start_philo(&table) != 0)
 		return (-1);
 	destroy_mutexes(&table, table.n_phil - 1);
 	return (ft_exit(NULL, &table, SUCCESS));
