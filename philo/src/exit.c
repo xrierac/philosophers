f4cc4fe7635924fe4fcd53a700c1373e92357ebe
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-14 11:26:46 +0200

    Add exit for exiting and cleaning functions

---
+++ philo/src/exit.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 09:53:00 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/03/14 11:14:26 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	ft_exit(char *s, t_table *table, t_status status)
+{
+	int	i;
+
+	i = -1;
+	if (status == ERROR)
+		ft_putstr_fd(s, 2);
+	if (table->philos)
+	{
+		while (table->philos[++i])
+			free(table->philos[i]);
+		free(table->philos);
+	}
+	//free(table);
+	return (status);
+}
