7f87a218a426aa1f03e35182c5809fd60b390702
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-15 17:19:57 +0200

    Add destroy_forks and collect_threads functions

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:41:00 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/14 16:37:12 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/15 16:36:21 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ int	main(int argc, char *argv[])
 
 	if (argc != 5 && argc != 6)
 		return (ft_exit(ERR_ARG_NUM, NULL, ERROR));
+	if (argc == 6 && argv[5] == 0)
+		return (0);
 	if (init_table(argc, argv, &table) == -1)
 		return (-1);
 	if (start_philo(&table) != 0)
 		return (-1);
-	destroy_mutexes(&table, table.n_phil - 1);
+	collect_threads(&table, table.n_phil - 1);
+	destroy_forks(&table, table.n_phil - 1);
 	return (ft_exit(NULL, &table, SUCCESS));
 }
