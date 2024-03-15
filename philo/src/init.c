52d46d37f8247f1b681eeb5ce7c140834255db3a
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-15 14:43:00 +0200

    Add init_fork function and correct fork assignment

--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:57:19 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/14 15:46:56 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/15 13:34:28 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 int	init_table(int argc, char *argv[], t_table *table)
 {
 	table->philos = NULL;
+	table->forks_lock = NULL;
 	if (parse_input(argv, argc, table) < 0)
 		return (ft_exit(ERR_ARG, table, ERROR));
 	if (sit_philos(table) < 0)
 		return (ft_exit(ERR_MALLOC, table, ERROR));
-	if (init_mutexes(table) != 0)
+	if (init_forks(table) != 0)
 		return (ft_exit(ERR_MUTEX_I, table, ERROR));
 	return (0);
 }
@@ -55,7 +56,22 @@ void	init_philos(t_table *table)
 		table->philos[i]->forks[1] = i + 1;
 		if (i == 0)
 			table->philos[i]->forks[0] = table->n_phil;
-		if (i == table->n_phil - 1)
-			table->philos[i]->forks[1] = 1;
+		table->philos[i]->table = table;
 	}
+}
+
+int	init_forks(t_table *table)
+{
+	int	i;
+
+	i = -1;
+	table->forks_lock = malloc(sizeof(pthread_mutex_t) * table->n_phil);
+	if (!table->forks_lock)
+		return (-1);
+	while (++i < table->n_phil)
+	{
+		if (pthread_mutex_init(&table->forks_lock[i], NULL))
+			return (destroy_forks(table, i));
+	}
+	return (0);
 }
