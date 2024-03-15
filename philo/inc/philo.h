a2a06f80ad6f2275c24d61c4f77397082c36e8bf
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-15 17:17:57 +0200

    Change time variables to long, add functions and error messages

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:39:52 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/14 16:46:21 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/15 15:24:16 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,32 @@
 # define ERR_MUTEX_I	"Mutex initialization failed\n"
 # define ERR_TIME		"Time function failed\n"
 # define ERR_COL_THR	"Closing threads failed\n"
+# define ERR_MUTEX_D	"Mutex destruction failed\n"
+
+typedef struct s_table t_table;
 
 typedef struct s_philo
 {
 	pthread_t		thread;
 	int				id;
 	int				n_meals;
-	int				last_meal;
+	long			last_meal;
 	int				forks[2];
 	pthread_mutex_t	meal_lock;
+	t_table			*table;
 }	t_philo;
 
 typedef struct s_table
 {
 	int		n_phil;
-	int		tdie;
-	int		teat;
-	int		tsleep;
-	int		must_eat;
-	int		start_time;
+	long	tdie;
+	long	teat;
+	long	tsleep;
+	long	must_eat;
+	long	start_time;
+	int		finish;
 	t_philo	**philos;
+	pthread_mutex_t	*forks_lock;
 }	t_table;
 
 typedef enum e_status
@@ -70,17 +76,21 @@ size_t	ft_strlen(char *str);
 long	ft_atol(char *str);
 
 //Mutexes//
+int		init_forks(t_table *table);
 int		init_mutexes(t_table *table);
+int		destroy_forks(t_table *table, int index);
 int		destroy_mutexes(t_table *table, int index);
 
 //Threads//
 int		start_philo(t_table *table);
-void	*philo_routine(void *arg);
+void	*main_routine(void *arg);
+int		collect_threads(t_table *table, int index);
 
 //Utils//
-int		get_time(void);
+long	get_time(void);
+void	precise_usleep(long time);
 void	ft_putstr_fd(char *s, int fd);
-t_table	*free_table(t_table *table);
+void	print_change(t_philo *philo, t_status status);
 
 //Exit//
 int	ft_exit(char *s, t_table *table, t_status status);
