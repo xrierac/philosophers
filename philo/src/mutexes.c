808c09952a3548fe35d0f3434f5f1634ad4766e2
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-20 16:53:30 +0200

    Change mutex configuration for better organization

--- philo/src/mutexes.c
+++ philo/src/mutexes.c
@@ -6,17 +6,30 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:14:48 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/20 15:29:33 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/20 16:53:04 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+int	destroy_mutexes(t_table *table, int index)
+{
+	int	i;
+
+	i = 0;
+	i += destroy_forks(table, index);
+	i += destroy_meals(table, index);
+	i += destroy_time(table, index);
+	if (pthread_mutex_destroy(&table->dead_lock))
+		i++;
+	return (i);
+}
+
 int	destroy_forks(t_table *table, int index)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	while (index > -1)
 	{
 		if (pthread_mutex_destroy(&table->forks_lock[index]))
@@ -26,33 +39,48 @@ int	destroy_forks(t_table *table, int index)
 	return (i);
 }
 
+int	destroy_meals(t_table *table, int index)
+{
+	while (index > -1)
+	{
+		if (pthread_mutex_init(&table->philos[index]->meal_lock))
+			return (1);
+		index--;
+	}
+	return (0);
+}
+
+int	destroy_time(t_table *table, int index)
+{
+	while (index > -1)
+	{
+		if (pthread_mutex_init(&table->philos[index]->time_lock))
+			return (1);
+		index--;
+	}
+	return (0);
+}
+
 int	init_mutexes(t_table *table)
 {
 	int	i;
 
-	i = 0;
-	while (i < table->n_phil)
+	i = -1;
+	while (++i < table->n_phil)
 	{
 		if (pthread_mutex_init(&table->philos[i]->meal_lock, NULL))
-			return (destroy_mutexes(table, i - 1, 1));
-		i++;
+			return (destroy_meals(table, i - 1), 1);
 	}
-	if (pthread_mutex_init(&table->time_lock, NULL)
-		  || pthread_mutex_init(&table->dead_lock, NULL))
-		  return (destroy_mutexes(table, i - 1, 0));
-	return (0);
-}
-
-int	destroy_mutexes(t_table *table, int index, int flag)
-{
-	while (index > -1)
+	i = -1;
+	while (++i < table->n_phil)
 	{
-		if (pthread_mutex_destroy(&table->philos[index]->meal_lock))
-			return (-2);
-		index--;
+		if (pthread_mutex_init(&table->philos[i]->time_lock, NULL))
+		{
+			destroy_meals(table, table->n_phil - 1);
+			return (destroy_time(table, i - 1), 1);
+		}
 	}
-	if (flag == 0 && (pthread_mutex_destroy(&table->time_lock)
-			|| pthread_mutex_destroy(&table->dead_lock)))
-			return (-2);
-	return (1);
+	if (pthread_mutex_init(&table->dead_lock, NULL))
+		return (1);
+	return (0);
 }
