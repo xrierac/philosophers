f8440b0df30e57b742a39b9821337c967826290d
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-20 11:53:17 +0200

    Add condition to avoid printing after someone dies

--- philo/src/print.c
+++ philo/src/print.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:44:29 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/15 15:12:13 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/20 11:43:54 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 void	print_change(t_philo *philo, t_status status)
 {
-	if (status == GOT_FORK)
-		printf("%ld %d has taken a fork\n", (get_time() - 
-				philo->table->start_time), philo->id);
-	if (status == EATING)
-		printf("%ld %d is eating\n", get_time() - philo->table->start_time,
+	if (philo->table->finish == 0)
+	{
+		if (status == GOT_FORK)
+			printf("%ld %d has taken a fork\n", (get_time()
+					- philo->table->start_time), philo->id);
+		if (status == EATING)
+			printf("%ld %d is eating\n", get_time() - philo->table->start_time,
 				philo->id);
-	if (status == THINK)
-		printf("%ld %d is thinking\n", get_time() - philo->table->start_time,
-				philo->id);
+		if (status == THINK)
+			printf("%ld %d is thinking\n", get_time()
+				- philo->table->start_time, philo->id);
+		if (status == SLEEP)
+			printf("%ld %d is sleeping\n", get_time()
+				- philo->table->start_time, philo->id);
+	}
 	if (status == DIED)
 		printf("%ld %d died\n", get_time() - philo->table->start_time,
-				philo->id);
-	if (status == SLEEP)
-		printf("%ld %d is sleeping\n", get_time() - philo->table->start_time,
-				philo->id);
+			philo->id);
 }
