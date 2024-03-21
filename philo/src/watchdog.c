4f21d8b4497d6bee2aa054a4844352588a7e58b8
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-21 12:16:56 +0200

    Improve check of death and check for full philosophers

--- philo/src/watchdog.c
+++ philo/src/watchdog.c
@@ -6,28 +6,38 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 14:04:21 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/21 09:23:39 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:51:33 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-static void	check_death(t_table *table)
+static int	check_table(t_table *table)
 {
 	int	i;
+	int	total_meals;
 
+	total_meals = 0;
 	i = -1;
 	while (++i < table->n_phil)
 	{
 		pthread_mutex_lock(&table->philos[i]->time_lock);
 		if (get_time() - table->philos[i]->last_meal >= table->tdie
-			&& table->finish == 0)
+			&& get_death_status(table) == 0)
 		{
+			pthread_mutex_lock(&table->dead_lock);
 			table->finish = 1;
+			pthread_mutex_unlock(&table->dead_lock);
 			print_change(table->philos[i], DIED);
 		}
 		pthread_mutex_unlock(&table->philos[i]->time_lock);
+		pthread_mutex_lock(&table->philos[i]->meal_lock);
+		total_meals += table->philos[i]->n_meals;
+		pthread_mutex_unlock(&table->philos[i]->meal_lock);
 	}
+	if (table->must_eat > 0 && total_meals >= table->must_eat * table->n_phil)
+		return (1);
+	return (0);
 }
 
 void	*watchdog(void *arg)
@@ -37,8 +47,9 @@ void	*watchdog(void *arg)
 	table = arg;
 	while (1)
 	{
-		check_death(table);
-		if (table->finish == 1)
+		if (check_table(table) == 1)
+			break ;
+		if (get_death_status(table))
 			break ;
 	}
 	return (NULL);
