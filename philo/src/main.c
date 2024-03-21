7b15541f784843475109def40f891d5a3230f370
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-21 12:33:11 +0200

    Add cleaning mutexes in case of error

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:41:00 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/21 12:04:10 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/21 12:32:22 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ int	main(int argc, char *argv[])
 	if (start_philo(&table) != 0)
 		return (-1);
 	if (collect_threads(&table, table.n_phil - 1) == -1)
-		return (-1);
+		return (destroy_mutexes(&table, table.n_phil - 1));
 	if (table.n_phil > 1)
 		if (pthread_join(table.watch, NULL))
-			return (ft_exit(ERR_COL_THR, &table, ERROR));
+			return (destroy_mutexes(&table, table.n_phil - 1),
+				ft_exit(ERR_COL_THR, &table, ERROR));
 	if (destroy_mutexes(&table, table.n_phil - 1) > 0)
 		return (ft_exit(ERR_MUTEX_D, &table, ERROR));
 	return (ft_exit(NULL, &table, SUCCESS));
